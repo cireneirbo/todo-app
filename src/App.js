3a74c15cda0ba5a6a406fc6e103e9f9e2eeb9ce7
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-09-18 22:50:02 -0400

    added some components

--- src/App.js
+++ src/App.js
@@ -22,10 +22,15 @@ class App extends Component{
       }
     ]
   }
+
+  markComplete = () => {
+    console.log('From app.js');
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,6 +3,8 @@ import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
+    
+
     render() {
         return this.props.todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />

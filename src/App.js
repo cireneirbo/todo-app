84c8f9e7d5dd772058be2aca8dcc43cae3f20d6a
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-09-19 22:11:58 -0400

    add some checkboxes

--- src/App.js
+++ src/App.js
@@ -23,8 +23,15 @@ class App extends Component{
     ]
   }
 
-  markComplete = () => {
-    console.log('From app.js');
+  //toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+    
   }
 
   render() {
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -12,11 +12,13 @@ export class TodoItem extends Component {
     }
 
     render() {
+        const { id, title} = this.props.todo;
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.props.markComplete} /> { ' ' }
-                    { this.props.todo.title }
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> { ' ' }
+                    { title }
+                    <button style={buttonStyle}>x</button>
                 </p>
             </div>
         )
@@ -28,4 +30,14 @@ TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
 }
 
+const buttonStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+
+}
 export default TodoItem
